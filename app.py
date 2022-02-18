cd62cfce9008886836c49d02bd293f257ef68b5b
msamwelmollel <47386502+msamwelmollel@users.noreply.github.com>
2022-02-18 10:40:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ Pipe = Pipeline([
 
 filename = 'questionanswer.pkl'
 
-# def read_filemodel():
-#     return pickle.load(open(filename, 'rb'))
+def read_filemodel():
+    return pickle.load(open(filename, 'rb'))
 
 #x = read_filemodel()
 
