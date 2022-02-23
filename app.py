551c6655a7b4ca51a194651dde155a96090138f4
msamwelmollel <47386502+msamwelmollel@users.noreply.github.com>
2022-02-23 20:28:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/get")
 def get_bot_response():
